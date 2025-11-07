6f48337b48d35a07e2a0adcd504528d3fa35d9ae
Y0urLocalCatboy <156523842+Y0urLocalCatboy@users.noreply.github.com>
2025-11-07 12:23:17 +0100

    Second excercise part 1

--- src/test/java/HRCalculatorTest.java
+++ src/test/java/HRCalculatorTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 import se2526.HRCalculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class HRCalculatorTest {
@@ -10,4 +11,16 @@ public class HRCalculatorTest {
         var result = HRCalculator.calculateMaxHR(25);
         assertEquals(195, result);
     }
+    @Test
+    public void testInvalidMinHR_ageTooHigh() {
+        assertThrows(IllegalArgumentException.class,
+                () -> HRCalculator.calculateMaxHR(500));
+    }
+    @Test
+    public void testInvalidMinHR_ageTooLow() {
+        assertThrows(IllegalArgumentException.class,
+                () -> HRCalculator.calculateMaxHR(-1));
+    }
+
 }
+
